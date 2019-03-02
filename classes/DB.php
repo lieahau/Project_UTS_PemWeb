bc07bd34cbeda941454222c6ea83d981cb6a2724
Budiman Putra Jaya <42717955+lieahau@users.noreply.github.com>
2019-03-02 22:54:15 +0700

    fix bugs and added queryInsert method

--- classes/DB.php
+++ classes/DB.php
@@ -1,15 +1,35 @@
 <?php
 class DB{
-    private static function connect(){
-        $pdo = new PDO("mysql:host='localhost';dbname='facenote';", 'root', ''); // connect pdo
+    public static function connect(){
+        $host = "localhost"; // lokasi mysql
+        $username = "root"; // user untuk login
+        $password = ""; // password untuk login
+        $dbname = "facenote"; // database name
+
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password); // connect pdo
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // set error mode
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
         return $pdo;
     }
 
     public static function query($query, $params = array()){
         $statement = self::connect()->prepare($query);
+        $statement->execute($params);
+    }
+
+    public static function queryInsert($tableName, $params = array()){
+        $columns = array_keys($params);
+        $values = array_values($params);
+        $query = "INSERT INTO $tableName (".implode(',', $columns).") VALUES ('".implode("', '", $values)."')";
+        $statement = self::connect()->prepare($query);
         $statement->execute();
+    }
+
+    public static function queryCount($tableName, $condition = true){
+        $query = "SELECT COUNT(*) FROM $tableName WHERE $condition";
+        $statement = self::connect()->query($query); // ini manggil query() function nya PDO, bukan function di class ini
+        return $statement->fetchColumn();
     }
 }
 
