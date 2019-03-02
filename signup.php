a27267f116c26c05f55b8a8cd3ca63d8ef959d99
Budiman Putra Jaya <42717955+lieahau@users.noreply.github.com>
2019-03-02 22:56:53 +0700

    Added dummy sign up form

--- signup.php
+++ signup.php
@@ -11,12 +11,47 @@
         <title>Facenote - Sign Up</title>
     </head>
     <body>
-
-
-
-
-
-
+        <form id="signupform" class="col-4" action="responses/signup-response.php" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username..." required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password..." required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="email@something.com" required>
+            </div>
+            <div class="form-group">
+                <label for="firstname">First Name</label>
+                <input type="text" class="form-control" id="firstname" name="firstname" placeholder="First Name..." required>
+            </div>
+            <div class="form-group">
+                <label for="lastname">Last Name</label>
+                <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Last Name...">
+            </div>
+            <div class="form-group">
+                <label for="birthdate">Birth Date</label>
+                <input type="date" class="form-control" id="birthdate" name="birthdate" required>
+            </div>
+            <div class="form-group">
+                <div class="input-radio">
+                    <input type="radio" id="male" name="sex" value="Male" checked> 
+                    <label for="male">Male</label>
+                </div>
+                <div class="input-radio">
+                    <input type="radio" id="female" name="sex" value="Female"> 
+                    <label for="female">Female</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="profilepict">Profile Picture</label>
+                <input type="file" class="form-control-file" id="profilepict" name="profilepict">
+            </form>
+            <button type="submit" name="submit-signup" class="btn btn-primary">Submit</button>
+        </form>
 
         <!-- Javascript Files -->
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
