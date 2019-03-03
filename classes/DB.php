4c12f46f8375fcf7e8e9c5fb9126c907b7bcd0a7
Budiman Putra Jaya <42717955+lieahau@users.noreply.github.com>
2019-03-03 12:46:14 +0700

    Added static variable pdo

--- classes/DB.php
+++ classes/DB.php
@@ -1,34 +1,43 @@
 <?php
 class DB{
+    public static $pdo;
+
     public static function connect(){
         $host = "localhost"; // lokasi mysql
         $username = "root"; // user untuk login
         $password = ""; // password untuk login
         $dbname = "facenote"; // database name
 
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password); // connect pdo
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // set error mode
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-        return $pdo;
+        self::$pdo = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password); // connect pdo
+        self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // set error mode
+        self::$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }
 
     public static function query($query, $params = array()){
-        $statement = self::connect()->prepare($query);
+        if(!isset(self::$pdo))
+            self::connect();
+
+        $statement = self::$pdo->prepare($query);
         $statement->execute($params);
     }
 
     public static function queryInsert($tableName, $params = array()){
+        if(!isset(self::$pdo))
+            self::connect();
+
         $columns = array_keys($params);
         $values = array_values($params);
         $query = "INSERT INTO $tableName (".implode(',', $columns).") VALUES ('".implode("', '", $values)."')";
-        $statement = self::connect()->prepare($query);
+        $statement = self::$pdo->prepare($query);
         $statement->execute();
     }
 
     public static function queryCount($tableName, $condition = true){
+        if(!isset(self::$pdo))
+            self::connect();
+
         $query = "SELECT COUNT(*) FROM $tableName WHERE $condition";
-        $statement = self::connect()->query($query); // ini manggil query() function nya PDO, bukan function di class ini
+        $statement = self::$pdo->query($query); // ini manggil query() function nya PDO, bukan function di class ini
         return $statement->fetchColumn();
     }
 }
