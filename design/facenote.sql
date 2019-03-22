fb4c7bfecddc7882b4d88dc6ca571179546c6dab
Budiman Putra Jaya <42717955+lieahau@users.noreply.github.com>
2019-03-22 19:40:36 +0700

    update sql database

--- design/facenote.sql
+++ design/facenote.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 22, 2019 at 12:00 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Host: localhost:3306
+-- Generation Time: Mar 22, 2019 at 12:39 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `facenote`
@@ -26,13 +28,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `comment`
 --
 
-CREATE TABLE IF NOT EXISTS `comment` (
+CREATE TABLE `comment` (
   `comment_id` int(11) NOT NULL,
   `comment_in` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `comment_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `comment_content` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `Date` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `comment`
@@ -60,28 +62,30 @@ INSERT INTO `comment` (`comment_id`, `comment_in`, `comment_by`, `comment_conten
 -- Table structure for table `status`
 --
 
-CREATE TABLE IF NOT EXISTS `status` (
+CREATE TABLE `status` (
   `status_id` int(11) NOT NULL,
-  `status_title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `writed_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `status_content` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `Date` date NOT NULL,
   `images` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `status`
 --
 
-INSERT INTO `status` (`status_id`, `status_title`, `writed_by`, `status_content`, `Date`, `images`) VALUES
-(0, '', 'willy@gmail.com', 'teeeeee', '2019-03-21', ''),
-(2, 'Wow', 'willy', 'ini namanya konten', '2019-03-05', 'uploads/altair.png'),
-(3, 'willy', 'willy@gmail.com', 'ini testing', '2019-03-21', 'uploads/'),
-(4, 'blabla', 'willy@gmail.com', 'wkjelreee', '2019-03-21', 'uploads/altair.png'),
-(6, '', 'willy@gmail.com', 'ini testing', '2019-03-21', ''),
-(7, '', 'test@gmail.com', 'testing', '2019-03-21', 'uploads/download.png'),
-(8, '', '', 'ff', '2019-03-21', 'uploads/Fast_8_quad.jpg'),
-(9, '', 'willy@gmail.com', 'ff', '2019-03-21', 'uploads/Fast_8_quad.jpg');
+INSERT INTO `status` (`status_id`, `writed_by`, `status_content`, `Date`, `images`) VALUES
+(0, 'willy@gmail.com', 'teeeeee', '2019-03-21', ''),
+(2, 'willy', 'ini namanya konten', '2019-03-05', 'uploads/altair.png'),
+(3, 'willy@gmail.com', 'ini testing', '2019-03-21', 'uploads/'),
+(4, 'willy@gmail.com', 'wkjelreee', '2019-03-21', 'uploads/altair.png'),
+(6, 'willy@gmail.com', 'ini testing', '2019-03-21', ''),
+(7, 'test@gmail.com', 'testing', '2019-03-21', 'uploads/download.png'),
+(8, '', 'ff', '2019-03-21', 'uploads/Fast_8_quad.jpg'),
+(9, 'willy@gmail.com', 'ff', '2019-03-21', 'uploads/Fast_8_quad.jpg'),
+(10, 'asdasda@asd.com', 'sss', '2019-03-22', 'uploads/'),
+(11, 'asdasda@asd.com', 'asdasdas', '2019-03-22', 'uploads/Akame Ga Kill Mine Happy.jpg'),
+(12, 'asdasda@asd.com', 'asdqwed', '2019-03-22', 'uploads/Akame Ga Kill Mine Sad.jpg');
 
 -- --------------------------------------------------------
 
@@ -89,7 +93,7 @@ INSERT INTO `status` (`status_id`, `status_title`, `writed_by`, `status_content`
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
@@ -99,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `birthdate` date NOT NULL,
   `sex` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Male',
   `profile_picture` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `user`
@@ -108,7 +112,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `birthdate`, `sex`, `profile_picture`) VALUES
 (1, 'willy', 'willy@gmail.com', 'ae2b1fca515949e5d54fb22b8ed95575', 'willy', 'chua', '2019-03-03', 'Male', 'uploads/altair.png'),
 (2, 'test', 'test@gmail.com', 'testing', 'test', 'lalala', '2019-03-05', 'Male', 'uploads/download.png'),
-(3, 'Donny', 'donny@gmail.com', 'apasaja', 'test', 'don', '2019-03-05', 'Male', 'uploads/download.png');
+(3, 'Donny', 'donny@gmail.com', 'apasaja', 'test', 'don', '2019-03-05', 'Male', 'uploads/download.png'),
+(4, 'liez', 'asdasd@asd.com', '977dcb62cf37d71f0255ab9b84d419f4', 'asdas', 'asdas', '2019-03-01', 'Male', 'placeholder.jpg'),
+(5, 'asdasdasd', 'asdasda@asd.com', '977dcb62cf37d71f0255ab9b84d419f4', 'asdasdsa', 'qweqwe', '2019-03-06', 'Male', 'placeholder.jpg');
 
 --
 -- Indexes for dumped tables
@@ -140,17 +146,21 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
 --
 -- AUTO_INCREMENT for table `status`
 --
 ALTER TABLE `status`
-  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
